f08667571206bee005fafe97b1ac32e9f111bd54
brunoais <brunoaiss@gmail.com>
2024-06-30 16:14:03 +0100

    Only make unit tests available if unittest is installed

--- get_image_size.py
+++ get_image_size.py
@@ -358,70 +358,73 @@ def get_image_metadata_from_bytesio(input, size, file_path=None):
                  height=height)
 
 
-import unittest
+try:
+    import unittest
+except ModuleNotFoundError:
+    pass
+else:
 
+    class Test_get_image_size(unittest.TestCase):
+        data = [{
+            'path': 'lookmanodeps.png',
+            'width': 251,
+            'height': 208,
+            'file_size': 22228,
+            'type': 'PNG'}]
 
-class Test_get_image_size(unittest.TestCase):
-    data = [{
-        'path': 'lookmanodeps.png',
-        'width': 251,
-        'height': 208,
-        'file_size': 22228,
-        'type': 'PNG'}]
+        def setUp(self):
+            pass
 
-    def setUp(self):
-        pass
+        def test_get_image_size_from_bytesio(self):
+            img = self.data[0]
+            p = img['path']
+            with io.open(p, 'rb') as fp:
+                b = fp.read()
+            fp = io.BytesIO(b)
+            sz = len(b)
+            output = get_image_size_from_bytesio(fp, sz)
+            self.assertTrue(output)
+            self.assertEqual(output,
+                             (img['width'],
+                              img['height']))
 
-    def test_get_image_size_from_bytesio(self):
-        img = self.data[0]
-        p = img['path']
-        with io.open(p, 'rb') as fp:
-            b = fp.read()
-        fp = io.BytesIO(b)
-        sz = len(b)
-        output = get_image_size_from_bytesio(fp, sz)
-        self.assertTrue(output)
-        self.assertEqual(output,
-                         (img['width'],
-                          img['height']))
+        def test_get_image_metadata_from_bytesio(self):
+            img = self.data[0]
+            p = img['path']
+            with io.open(p, 'rb') as fp:
+                b = fp.read()
+            fp = io.BytesIO(b)
+            sz = len(b)
+            output = get_image_metadata_from_bytesio(fp, sz)
+            self.assertTrue(output)
+            for field in image_fields:
+                self.assertEqual(getattr(output, field), None if field == 'path' else img[field])
 
-    def test_get_image_metadata_from_bytesio(self):
-        img = self.data[0]
-        p = img['path']
-        with io.open(p, 'rb') as fp:
-            b = fp.read()
-        fp = io.BytesIO(b)
-        sz = len(b)
-        output = get_image_metadata_from_bytesio(fp, sz)
-        self.assertTrue(output)
-        for field in image_fields:
-            self.assertEqual(getattr(output, field), None if field == 'path' else img[field])
+        def test_get_image_metadata(self):
+            img = self.data[0]
+            output = get_image_metadata(img['path'])
+            self.assertTrue(output)
+            for field in image_fields:
+                self.assertEqual(getattr(output, field), img[field])
 
-    def test_get_image_metadata(self):
-        img = self.data[0]
-        output = get_image_metadata(img['path'])
-        self.assertTrue(output)
-        for field in image_fields:
-            self.assertEqual(getattr(output, field), img[field])
+        def test_get_image_metadata__ENOENT_OSError(self):
+            with self.assertRaises(OSError):
+                get_image_metadata('THIS_DOES_NOT_EXIST')
 
-    def test_get_image_metadata__ENOENT_OSError(self):
-        with self.assertRaises(OSError):
-            get_image_metadata('THIS_DOES_NOT_EXIST')
+        def test_get_image_metadata__not_an_image_UnknownImageFormat(self):
+            with self.assertRaises(UnknownImageFormat):
+                get_image_metadata('README.rst')
 
-    def test_get_image_metadata__not_an_image_UnknownImageFormat(self):
-        with self.assertRaises(UnknownImageFormat):
-            get_image_metadata('README.rst')
+        def test_get_image_size(self):
+            img = self.data[0]
+            output = get_image_size(img['path'])
+            self.assertTrue(output)
+            self.assertEqual(output,
+                             (img['width'],
+                              img['height']))
 
-    def test_get_image_size(self):
-        img = self.data[0]
-        output = get_image_size(img['path'])
-        self.assertTrue(output)
-        self.assertEqual(output,
-                         (img['width'],
-                          img['height']))
-
-    def tearDown(self):
-        pass
+        def tearDown(self):
+            pass
 
 
 def main(argv=None):
